70317fd72527a0696725f9d5db10c77a4e4f396c
prach <matej.kratochvil@student.gyarab.cz>
2024-08-18 22:52:52 +0200

    Posts + Server-Side loading

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import PostEditor from "@/components/posts/editor/PostEditor";
+import Post from "@/components/posts/Post";
+import prisma from "@/lib/prisma";
+import { PostDataInclude } from "@/lib/types";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: PostDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
-    <main>
-      <div className="w-full">Front Page</div>
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
     </main>
   );
 }
