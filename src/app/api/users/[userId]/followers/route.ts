4a75725ee9c90140c8e6c9ea8ff6629f739d36e0
prach <matej.kratochvil@student.gyarab.cz>
2024-08-21 21:58:39 +0200

    Follow Feature

--- src/app/api/users/[userId]/followers/route.ts
+++ src/app/api/users/[userId]/followers/route.ts
@@ -56,21 +56,20 @@ export async function POST(
     }
 
     await prisma.follow.upsert({
-        where: {
-            followerId_followingId: {
-                followerId: loggedInUser.id,
-                followingId: userId
-            }
+      where: {
+        followerId_followingId: {
+          followerId: loggedInUser.id,
+          followingId: userId,
         },
-        create: {
-            followerId: loggedInUser.id,
-            followingId: userId
-        },
-        update: {}
-    })
+      },
+      create: {
+        followerId: loggedInUser.id,
+        followingId: userId,
+      },
+      update: {},
+    });
 
     return new Response();
-
   } catch (error) {
     console.log(error);
     return Response.json({ error: "Internal Server Error" }, { status: 500 });
@@ -91,12 +90,11 @@ export async function DELETE(
     await prisma.follow.deleteMany({
       where: {
         followerId: loggedInUser.id,
-        followingId: userId
-      }
-    })
+        followingId: userId,
+      },
+    });
 
     return new Response();
-    
   } catch (error) {
     console.log(error);
     return Response.json({ error: "Internal Server Error" }, { status: 500 });
