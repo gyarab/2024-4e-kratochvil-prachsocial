24b33457e5c786087a9d85c24b8a183273bb0b80
prach <matej.kratochvil@student.gyarab.cz>
2024-08-22 18:55:17 +0200

    Loading + 404 page

---
+++ src/app/(main)/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <main className="my-12 w-full space-y-3 text-center">
+        <h1 className="text-3xl font-bold">Not Found</h1>
+        <p>Hmm... The page you were looking for doesn&apos;t exist.</p>
+      </main>
+    );
+  }
