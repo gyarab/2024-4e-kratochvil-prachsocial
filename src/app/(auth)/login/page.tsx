0fa3df8f85710b092de75fc12b87209982f8b26b
prach <matej.kratochvil@student.gyarab.cz>
2024-11-23 19:45:06 +0100

    Removed Form Images

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,6 @@
 import { Metadata } from "next";
 import LoginForm from "./LoginForm";
 import Link from "next/link";
-import loginImage from "@/assets/login-image.jpg";
-import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -11,8 +9,8 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-5">
-      <div className="rounded-2x1 flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden bg-card shadow-2xl">
-        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+      <div className="rounded-2x1 flex h-full max-h-[40rem] w-full max-w-[32rem] overflow-hidden bg-card shadow-2xl">
+        <div className="w-full space-y-10 overflow-y-auto p-10">
           <h1 className="text-3x1 text-center">Login</h1>
           <div className="space-y-5">
             <LoginForm />
@@ -21,11 +19,6 @@ export default function Page() {
             </Link>
           </div>
         </div>
-        <Image
-          src={loginImage}
-          alt="Login image"
-          className="hidden w-1/2 object-cover md:block"
-        />
       </div>
     </main>
   );
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,4 @@
 import { Metadata } from "next";
-import signupImage from "@/assets/signup-image.jpg";
-import Image from "next/image";
 import Link from "next/link";
 import SignUpForm from "./SignUpForm";
 
@@ -11,8 +9,8 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-5">
-      <div className="rounded-2x1 flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden bg-card shadow-2xl">
-        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+      <div className="rounded-2x1 flex h-full max-h-[40rem] w-full max-w-[32rem] overflow-hidden bg-card shadow-2xl">
+        <div className="w-full space-y-10 overflow-y-auto p-10">
           <div className="space-y-1 text-center">
             <h1 className="text-3xl font-bold">Sign Up</h1>
             <p className="text-muted-foreground">
@@ -26,11 +24,6 @@ export default function Page() {
             </Link>
           </div>
         </div>
-        <Image
-          src={signupImage}
-          alt="Signup image"
-          className="hidden w-1/2 object-cover md:block"
-        />
       </div>
     </main>
   );
